b82ecdd24277bd314d5a2fe6aa68c9bc78b4851d
torco34 <torcoromaarias@gmail.com>
2024-11-12 13:28:02 -0500

    nuevo importaion

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,4 @@
-// const { defineConfig } = require("@vue/cli-service");
-// module.exports = defineConfig({
-//   transpileDependencies: true,
-//   css: {
-//     loaderOptions: {
-//       sass: {
-//         additionalData: `@import "@/scss/_variables.scss";`,
-//       },
-//     },
-//   },
-// });
-// // (Inside vue.config.js)
-// module.exports = {
-//   publicPath: process.env.NODE_ENV === 'production' ? '/nombre-repositorio/' : '/'
-// }
+
 const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
